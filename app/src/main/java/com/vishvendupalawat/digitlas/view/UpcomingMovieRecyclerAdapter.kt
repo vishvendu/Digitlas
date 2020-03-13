2512997edc57dec7e80807b48c196357b22a21a0
vishvendu <vishal.palawat785@gmail.com>
2020-03-13 09:49:38 +0530

    updated adapter upcoming movie

--- app/src/main/java/com/vishvendupalawat/digitlas/view/UpcomingMovieRecyclerAdapter.kt
+++ app/src/main/java/com/vishvendupalawat/digitlas/view/UpcomingMovieRecyclerAdapter.kt
@@ -1,9 +1,15 @@
 package com.vishvendupalawat.digitlas.view
 
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvm_master.Model.UpcomingMovieResponse
+import com.squareup.picasso.Picasso
+import com.vishvendupalawat.digitlas.R
+import kotlinx.android.synthetic.main.upcoming_movie_item_list.view.*
 
 class UpcomingMovieRecyclerAdapter(upcomingmovie : ArrayList<UpcomingMovieResponse>) : RecyclerView.Adapter<UpcomingMovieRecyclerAdapter.UpcomingMovieViewHolder>() {
 
@@ -11,7 +17,9 @@ class UpcomingMovieRecyclerAdapter(upcomingmovie : ArrayList<UpcomingMovieRespon
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UpcomingMovieViewHolder {
         // inflate layout here
-
+        return UpcomingMovieViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.upcoming_movie_item_list,parent,false)
+        )
 
     }
 
@@ -26,7 +34,12 @@ class UpcomingMovieRecyclerAdapter(upcomingmovie : ArrayList<UpcomingMovieRespon
 
     class UpcomingMovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+            val movie_image : ImageView = itemView.imageview
 
+        fun bind(upcomingMovieResponse: UpcomingMovieResponse){
+
+            Picasso.with(context).load(imageUri).into(upcomingMovieResponse.results.);
+        }
 
     }
 
